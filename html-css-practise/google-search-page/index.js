21cb939eaac47a1539d243e44be6739d5a56d6a6
abc <rajat2502@users.noreply.github.com>
2019-06-27 14:03:34 +0530

    tried js documentation page

--- html-css-practise/google-search-page/index.js
+++ html-css-practise/google-search-page/index.js
@@ -18,7 +18,15 @@ mic.addEventListener('mouseout', function(){
 
 // function myFunction() {
 //   if (window.scrollY > 132) {
-//     document.querySelector('header').style.position = "fixed";
-//     document.querySelector('header').style.background = "white";
+//     document.querySelector('nav').style.position = "fixed";
+//     document.querySelector('nav').style.background = "white";
+//     document.querySelector('nav').style.marginTop = 0;
+//     document.querySelector('nav').style.boxShadow = "2px 2px 2px #888888";    
+//   }
+//   else{
+//     document.querySelector('nav').style.position = "flex";
+//     document.querySelector('nav').style.background = "white";
+//     document.querySelector('nav').style.marginTop = 16;
+//     document.querySelector('nav').style.boxShadow = 0;    
 //   }
 // }
