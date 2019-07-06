3b588bdbf9a1a791628037c34bc686ce5ecd8e6d
abc <rajat2502@users.noreply.github.com>
2019-07-06 14:18:00 +0530

    small features added to momentum extension

--- chrome-extensions/show-time/main.js
+++ chrome-extensions/show-time/main.js
@@ -7,14 +7,46 @@
 //   }
 // })
 
+// set the time on opening
+var today = new Date();
+  hour = today.getHours();
+  min = today.getMinutes();
+  console.log(today.getSeconds());
+
+  document.getElementById('hour').innerHTML = 
+  `${hour}`;
+
+  if(min < 10){
+    document.getElementById('min').innerHTML = 
+  `0${min}`;
+  }
+  else{
+  document.getElementById('min').innerHTML = 
+  `${min}`;
+  }
+
+// check and change the time
 setInterval(function(){
   var today = new Date();
   hour = today.getHours();
   min = today.getMinutes();
-  console.log(today.getSeconds());
+
   document.getElementById('hour').innerHTML = 
   `${hour}`;
+
+  if(min < 10){
+    document.getElementById('min').innerHTML = 
+  `0${min}`;
+  }
+  else{
   document.getElementById('min').innerHTML = 
   `${min}`;
+  }
+
 }, 1000);
 
+random = Math.floor(Math.random() * 6) + 1;
+imgaddress = "images/" + random + ".jpg";
+console.log(imgaddress);
+document.body.background = imgaddress;
+ 
