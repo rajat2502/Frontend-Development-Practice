4850705e1dc5d70f80c86d924c87897c6c42daee
abc <rajat2502@users.noreply.github.com>
2019-07-07 11:29:47 +0530

    learned higher order functions in js

---
+++ javascript practise/higher-order-functions.js
@@ -0,0 +1,59 @@
+// we can assign fucntions to variables
+
+const announceThatIAmDoingImportantWork = () => {
+  console.log("I’m doing very important work!");
+};  //very huge name 
+
+// assign it to any variable
+const busy = announceThatIAmDoingImportantWork;
+
+busy(); // This function call barely takes any space!
+
+// In JavaScript, functions are first class objects. so they have properties suck as .length , .name , and methods such as .toString()
+// for eexample
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+  for(let i = 1; i <= 1000000; i++) {
+    if ( (2 + 2) != 4) {
+      console.log('Something has gone very wrong :( ');
+    }
+  }
+}
+
+// Write your code below
+const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+is2p2();
+console.log(is2p2.name) //it will print the actual name of the function
+
+//function can also be passed as parameters to other functions
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+  for(let i = 1; i <= 1000000; i++) {
+    if ( (2 + 2) != 4) {
+      console.log('Something has gone very wrong :( ');
+    }
+  }
+};
+
+const addTwo = num => num + 2;
+
+const timeFuncRuntime = funcParameter => {
+  let t1 = Date.now();
+  funcParameter();
+  let t2 = Date.now();
+  return t2 - t1;
+};
+
+// Write your code below
+
+const time2p2 = timeFuncRuntime(checkThatTwoPlusTwoEqualsFourAMillionTimes);
+
+const checkConsistentOutput = (func, val) => {
+    let firstTry = func(val);
+    let secondTry = func(val);
+    if (firstTry === secondTry) {
+        return firstTry
+    } else {
+        return 'This function returned inconsistent results'
+    }
+};
+
+checkConsistentOutput(addTwo, 10);
