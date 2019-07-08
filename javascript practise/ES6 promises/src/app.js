3a9da12138072001a10942a73a9a2ce78199640d
abc <rajat2502@users.noreply.github.com>
2019-07-08 17:24:00 +0530

    revised ES6 promises

--- javascript practise/ES6 promises/src/app.js
+++ javascript practise/ES6 promises/src/app.js
@@ -60,3 +60,42 @@ createPost({title: 'Post three', body: 'This is Post three'})
 .catch(function(err){
   console.log(err);
 });
+
+// promise declararion
+const inventory = {
+  sunglasses: 1900,
+  pants: 1088,
+  bags: 1344
+};
+
+// Write your code below:
+const myExecutor = (resolve, reject) => {
+  if(inventory.sunglasses > 0){
+    resolve('Sunglasses order processed.');
+  }
+  else{
+    reject('That item is sold out.');
+  }
+}
+
+const orderSunglasses = () => { 
+  return new Promise(myExecutor);
+}
+
+const orderPromise = orderSunglasses();
+console.log(orderPromise);
+
+// settimeout async
+console.log("This is the first line of code in app.js.");
+// Keep the line above as the first line of code
+// Write your code here:
+
+const usingSTO = () => {
+  console.log('hi i am in settimeout');
+}
+setTimeout(usingSTO, 1000);
+
+// Keep the line below as the last line of code:
+console.log("This is the last line of code in app.js.");
+
+// consuming promises
